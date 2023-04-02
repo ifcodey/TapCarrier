f7d770b30827014cb4d8d3d4a7a8cbc5f92b8e7e
ifcodey <ifcodey@gmail.com>
2023-04-02 22:43:00 +0300

    done

--- Challenge 2/Challenge.js
+++ Challenge 2/Challenge.js
@@ -1,4 +1,63 @@
 
+let arr = [2,2,1,1,1,2,2];
+// let arr = [3,2,3];
+let res = 0, count = 0;
+
+for (let i = 0; i < arr.length; i++) {
+
+    if (count == 0) {
+        res = arr[i];
+        count++;
+    } if (count > 0) {
+
+        if (res == arr[i]) {
+            count++;
+        } else {
+            count -= 1;
+        };
+    }
+
+}
+
+if (count == 0) {
+    res = arr[arr.length - 1];
+} else if(count == 1){
+    res = -1;
+}
+
+
+console.log(res);
+
+//>>>
+//>>>>>
+//>>>>>>>
+//>>>>>>>>>
+//>>>>>>>
+//>>>>>
+//>>>
+
+// function findMajority(arr, n) {
+//     return arr[Math.floor(n / 2)];
+// }
+
+// // driver code
+
+// let arr = [3, 2, 3,];
+// let n = arr.length;
+// // console.log(arr.sort());
+// console.log(findMajority(arr.sort((first, last) => {
+//     return Math.abs(first) - Math.abs(last);
+// }
+// ), n));
+
+//>>>
+//>>>>>
+//>>>>>>>
+//>>>>>>>>>
+//>>>>>>>
+//>>>>>
+//>>>
+
 // function majorityElement(nums) {
 
 //     let count = {}; //object
@@ -24,35 +83,3 @@
 
 
 
-
-
-let arr = [3, 2, 3];
-
-let count = 0;
-
-for (let i = 0; 0 < arr.length; i++) {
-    if (arr[i]) {
-        sort_arr[i] = arr[i];
-        count++;
-    }
-}
-
-
-
-
-
-
-
-// function findMajority(arr, n) {
-//     return arr[Math.floor(n / 2)];
-// }
-
-// // driver code
-
-// let arr = [3, 2, 3,];
-// let n = arr.length;
-// // console.log(arr.sort());
-// console.log(findMajority(arr.sort((first, last) => {
-//     return Math.abs(first) - Math.abs(last);
-// }
-// ), n));
